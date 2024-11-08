a04ca9b0e295bf788701de1a214fe474f8880878
Sai Akshith <96678331+SaiAkshith-2001@users.noreply.github.com>
2024-11-08 19:10:14 +0530

    initial commit

---
+++ java/1.TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+public class Main
+{
+	public static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> numMap = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (numMap.containsKey(complement)) {
+				return new int[] { numMap.get(complement), i };
+			}
+			numMap.put(nums[i], i);
+		}
+		return new int[] {};//empty array where no two sum is possible
+		//throw new IllegalArgumentException("No two sum solution");
+	}
+	public static void main(String[] args) {
+		int[] nums= {1,2,3,4,5,6};
+		System.out.println(Arrays.toString(twoSum(nums,7)));
+	}
+}
