038a5ec2f23d4ca8ff6e75ecd5b0c764ad595513
Sai Akshith <96678331+SaiAkshith-2001@users.noreply.github.com>
2024-11-13 22:16:30 +0530

    initial commit

---
+++ java/dp/Factorial.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static int factorial(int n) {
+		int[] dp=new int[n+1];
+		dp[0]=1;
+		for(int i=1; i<=n; i++) {
+			dp[i]=dp[i-1]*i;
+		}
+		return dp[n];
+	}
+	public static void main(String[] args) {
+		System.out.println(factorial(5));
+	}
+}
