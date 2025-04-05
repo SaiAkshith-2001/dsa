e287e06bffcb9e2c44b03b6eb7803dc5949bcb80
Sai Akshith <96678331+SaiAkshith-2001@users.noreply.github.com>
2025-04-05 15:44:49 +0530

    updated maxSum after initial currentSum

--- patterns/Sliding Window/MaxSubArray.java
+++ patterns/Sliding Window/MaxSubArray.java
@@ -8,6 +8,7 @@ public class Main {
 		for(int i=0; i<k; i++) {
 			currentSum+=arr[i];
 		}
+		maxSum=currentSum;
 		for(int i=k; i<arr.length; i++) {
 			currentSum+=arr[i]-arr[i-k];        //adding next elements to the window and removing the prev first elements form the window
 			maxSum=Math.max(maxSum,currentSum);
