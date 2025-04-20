ea6e38a130d13aaad4595caf341cae55d93314cf
Sai Akshith <96678331+SaiAkshith-2001@users.noreply.github.com>
2025-04-20 21:01:10 +0530

    updated for insertion and deletion for NthIndex

--- patterns/LinkedList/DoublyLL.java
+++ patterns/LinkedList/DoublyLL.java
@@ -38,9 +38,44 @@ public class DoublyLL
 		tail.next=newNode;
 		tail=newNode;
 	}
+	public void pushElementAtNthIndex(int data,int index) {
+		if(index<=0) throw new IllegalArgumentException("Index must be greater than zero");
+		System.out.println("\n\nafter insertion at index: "+index);
+		Node newNode=new Node(data);
+		if(index==1) {  //inserting at head
+			newNode.next=head;
+			if(head!=null) {
+				head.prev=newNode;
+			}
+			head=newNode;
+			if(tail==null) {
+				tail=newNode;
+			}
+			return;
+		}
+		Node currentNode=head;
+		int currentIndex=1;
+		while(currentNode!=null&&currentIndex<index-1) {
+			currentNode=currentNode.next;
+			currentIndex++;
+		}
+		if(currentIndex==index-1) {
+			newNode.prev=currentNode;
+			newNode.next=currentNode.next;
+			if(currentNode.next!=null) {
+				currentNode.next.prev=newNode;
+			}
+			else {
+				tail=newNode;
+			}
+			currentNode.next=newNode;
+		} else {
+			throw new IndexOutOfBoundsException("Index exceeds size of the linkedlist");
+		}
+	}
 	public Node popFront() {
 		if(head==null) {
-			System.out.println("list is empty");
+			System.out.println("list is empty!");
 			return null;
 		}
 		Node tempNode=head;
@@ -53,7 +88,7 @@ public class DoublyLL
 	}
 	public Node popBack() {
 		if(head==null) {
-			System.out.println("list is empty");
+			System.out.println("list is empty!");
 			return null;
 		}
 		Node tempNode=tail;
@@ -63,6 +98,49 @@ public class DoublyLL
 			tail.next=null;
 		}
 		return tail;
+	}
+	public void popElementAtNthIndex(int index) {
+		if (index <= 0) {
+			throw new IllegalArgumentException("Index must be greater than zero");
+		}
+		System.out.println("\n\nafter deletion at index: " + index);
+		if (head == null) {
+			System.out.println("List is empty!");
+			return;
+		}
+		if (index == 1) {
+			head = head.next;
+			if (head != null) {
+				head.prev = null;
+			} else {
+				tail = null;
+			}
+			size--;
+			return;
+		}
+
+		Node currentNode = head;
+		int currentIndex = 1; // Start with 1-based indexing
+
+		while (currentNode != null && currentIndex < index - 1) {
+			currentNode = currentNode.next;
+			currentIndex++;
+		}
+
+		if (currentNode == null || currentNode.next == null) {
+			throw new IndexOutOfBoundsException("Index exceeds the size of the linked list");
+		}
+
+		Node nodeToDelete = currentNode.next;
+		currentNode.next = nodeToDelete.next;
+
+		if (nodeToDelete.next != null) {
+			nodeToDelete.next.prev = currentNode;
+		} else {
+			tail = currentNode;
+		}
+
+		size--;
 	}
 	public void forwardTraversal() {
 		if(head==null) {
@@ -102,6 +180,16 @@ public class DoublyLL
 		dll.popFront();
 		dll.popBack();
 		System.out.println("\n\nafter deletion:");
+		dll.forwardTraversal();
+		System.out.println("\n");
+		dll.backwardTraversal();
+		System.out.println("\nlength of DLL: "+dll.getSize());
+		dll.pushElementAtNthIndex(1000,11);
+		dll.forwardTraversal();
+		System.out.println("\n");
+		dll.backwardTraversal();
+		System.out.println("\nlength of DLL: "+dll.getSize());
+		dll.popElementAtNthIndex(11);
 		dll.forwardTraversal();
 		System.out.println("\n");
 		dll.backwardTraversal();
